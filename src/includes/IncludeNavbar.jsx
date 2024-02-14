5b4fd554733eb339e681b755c24dd6f58c83ba43
tortong <kittithat.charoenchet101@gmail.com>
2024-02-14 17:12:54 +0700

    update navbar and add icon to navbar

--- src/includes/IncludeNavbar.jsx
+++ src/includes/IncludeNavbar.jsx
@@ -1,3 +1,4 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 function IncludeNavbar()
 {
     return (
@@ -5,12 +6,12 @@ function IncludeNavbar()
             <nav className="wrapper-navbar">
                 <ul>
                     <li><i className='fas fa-home'></i> หน้าแรก</li>
-                    <li>อัลบั้ม</li>
-                    <li>ทีมงาน</li>
-                    <li>ข่าวสาร</li>
-                    <li>รีวิว</li>
-                    <li>เกี่ยวกับ</li>
-                    <li>กระทู้</li>
+                    <li><i class="fa-solid fa-magnifying-glass-location"></i> รีวิว</li>
+                    <li><i class="fa-solid fa-newspaper"></i> ข่าวสาร</li>
+                    <li><i class="fa-solid fa-people-group"></i> ทีมงาน</li>
+                    <li><i class="fa-solid fa-user-plus"></i> สมัครสมาชิก</li>
+                    <li><i class="fa-solid fa-arrow-right-to-bracket"></i> เข้าสู่ระบบ</li>
+                    <li> <i class="fa-regular fa-address-card"></i> เกี่ยวกับ</li>
                 </ul>
             </nav>        
         </>
