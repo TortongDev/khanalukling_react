9fe8dc3b4619b975759fef34d34376b6f4614117
tortong <kittithat.charoenchet101@gmail.com>
2024-02-16 15:45:24 +0700

    update style main page

--- src/pages/PageHome.jsx
+++ src/pages/PageHome.jsx
@@ -18,33 +18,37 @@ function PageHome (){
     })
     
     return(<>
-        <IncludeHeader />
-        <IncludeNavbar />
-        <IncludeNavbarMobile />
-        <div className="wrapper-slide">
-            <div class="fade">
-                <div className="flex">
-                    <h2>Header 1</h2>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam, quasi! Placeat incidunt nobis, ratione autem ipsam, aliquid delectus aspernatur ea, cupiditate sit minus odit. Iure est, porro amet eius fugiat ducimus quas ipsa voluptatem, harum odit exercitationem natus ipsam dolor eligendi animi, illo minima repellendus sunt vitae vero officia consectetur provident? Rem laudantium ipsam sapiente, tenetur odio quos eum deserunt ipsa quod assumenda sed voluptatem suscipit cupiditate officia quia in magnam quibusdam, voluptatum repudiandae exercitationem! Velit impedit iste similique eligendi architecto earum nulla. Modi, commodi dolorum? Dolor numquam eligendi quam odit, molestias architecto molestiae harum sint, illum ad obcaecati deleniti.
-                </div>
-                <div className="flex">
-                    <h2>Header 2</h2>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam, quasi! Placeat incidunt nobis, ratione autem ipsam, aliquid delectus aspernatur ea, cupiditate sit minus odit. Iure est, porro amet eius fugiat ducimus quas ipsa voluptatem, harum odit exercitationem natus ipsam dolor eligendi animi, illo minima repellendus sunt vitae vero officia consectetur provident? Rem laudantium ipsam sapiente, tenetur odio quos eum deserunt ipsa quod assumenda sed voluptatem suscipit cupiditate officia quia in magnam quibusdam, voluptatum repudiandae exercitationem! Velit impedit iste similique eligendi architecto earum nulla. Modi, commodi dolorum? Dolor numquam eligendi quam odit, molestias architecto molestiae harum sint, illum ad obcaecati deleniti.
-                </div> 
-                <div className="flex">
-                    <h2>Header 3</h2>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam, quasi! Placeat incidunt nobis, ratione autem ipsam, aliquid delectus aspernatur ea, cupiditate sit minus odit. Iure est, porro amet eius fugiat ducimus quas ipsa voluptatem, harum odit exercitationem natus ipsam dolor eligendi animi, illo minima repellendus sunt vitae vero officia consectetur provident? Rem laudantium ipsam sapiente, tenetur odio quos eum deserunt ipsa quod assumenda sed voluptatem suscipit cupiditate officia quia in magnam quibusdam, voluptatum repudiandae exercitationem! Velit impedit iste similique eligendi architecto earum nulla. Modi, commodi dolorum? Dolor numquam eligendi quam odit, molestias architecto molestiae harum sint, illum ad obcaecati deleniti.
+        <div className="clearfix">
+            <IncludeHeader />
+            <IncludeNavbar />
+            <IncludeNavbarMobile />
+            <div className="wrapper-slide">
+                <div className="banner"></div>
+                <div class="fade">
+                    <div className="flex">
+                        <h2>Header 1</h2>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam, quasi! Placeat incidunt nobis, ratione autem ipsam, aliquid delectus aspernatur ea, cupiditate sit minus odit. Iure est, porro amet eius fugiat ducimus quas ipsa voluptatem, harum odit exercitationem natus ipsam dolor eligendi animi, illo minima repellendus sunt vitae vero officia consectetur provident? Rem laudantium ipsam sapiente, tenetur odio quos eum deserunt ipsa quod assumenda sed voluptatem suscipit cupiditate officia quia in magnam quibusdam, voluptatum repudiandae exercitationem! Velit impedit iste similique eligendi architecto earum nulla. Modi, commodi dolorum? Dolor numquam eligendi quam odit, molestias architecto molestiae harum sint, illum ad obcaecati deleniti.
+                    </div>
+                    <div className="flex">
+                        <h2>Header 2</h2>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam, quasi! Placeat incidunt nobis, ratione autem ipsam, aliquid delectus aspernatur ea, cupiditate sit minus odit. Iure est, porro amet eius fugiat ducimus quas ipsa voluptatem, harum odit exercitationem natus ipsam dolor eligendi animi, illo minima repellendus sunt vitae vero officia consectetur provident? Rem laudantium ipsam sapiente, tenetur odio quos eum deserunt ipsa quod assumenda sed voluptatem suscipit cupiditate officia quia in magnam quibusdam, voluptatum repudiandae exercitationem! Velit impedit iste similique eligendi architecto earum nulla. Modi, commodi dolorum? Dolor numquam eligendi quam odit, molestias architecto molestiae harum sint, illum ad obcaecati deleniti.
+                    </div> 
+                    <div className="flex">
+                        <h2>Header 3</h2>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam, quasi! Placeat incidunt nobis, ratione autem ipsam, aliquid delectus aspernatur ea, cupiditate sit minus odit. Iure est, porro amet eius fugiat ducimus quas ipsa voluptatem, harum odit exercitationem natus ipsam dolor eligendi animi, illo minima repellendus sunt vitae vero officia consectetur provident? Rem laudantium ipsam sapiente, tenetur odio quos eum deserunt ipsa quod assumenda sed voluptatem suscipit cupiditate officia quia in magnam quibusdam, voluptatum repudiandae exercitationem! Velit impedit iste similique eligendi architecto earum nulla. Modi, commodi dolorum? Dolor numquam eligendi quam odit, molestias architecto molestiae harum sint, illum ad obcaecati deleniti.
+                    </div>
                 </div>
             </div>
         </div>
         <div className="tabbar-title">
-            <h4 style={{margin:'0px'}}>รีวิวมาใหม่ <img src="new.webp" height={'30px'} alt="" /></h4>
+            {/* test style  */}
+            <h4 className="ico-action" style={{margin:'0px'}}>รีวิวมาใหม่ <img src={process.env.PUBLIC_URL+"/new.ico"} style={{"width":"30px","margin-top":"-3px"}} alt="" /></h4>
             <h4><i class="fa-solid fa-up-right-from-square"></i></h4>
         </div>
         <article className="wrapper-gallery">
             <section>
                 <div className="img-gallery">
-                    <img src="/khanalukling_react/public/images/banner/LINE_ALBUM_3224_240214_1.jpg" alt="" />
+                    <img src={process.env.PUBLIC_URL+'/images/gallery/LINE_ALBUM_3224_240214_1.jpg'} width={'100%'} height={'150px'} alt="" />
                 </div>
                 <div className="text-gallery">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, sequi!
@@ -52,7 +56,7 @@ function PageHome (){
             </section>
             <section>
                 <div className="img-gallery">
-                    <img src="/khanalukling_react/public/images/banner/LINE_ALBUM_3224_240214_1.jpg" alt="" />
+                    <img src={process.env.PUBLIC_URL+'/images/gallery/LINE_ALBUM_3224_240214_1.jpg'} width={'100%'} height={'150px'} alt="" />
                 </div>
                 <div className="text-gallery">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, sequi!
@@ -60,7 +64,7 @@ function PageHome (){
             </section>
             <section>
                 <div className="img-gallery">
-                    <img src="/khanalukling_react/public/images/banner/LINE_ALBUM_3224_240214_1.jpg" alt="" />
+                    <img src={process.env.PUBLIC_URL+'/images/gallery/LINE_ALBUM_3224_240214_1.jpg'} width={'100%'} height={'150px'} alt="" />
                 </div>
                 <div className="text-gallery">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, sequi!
@@ -68,7 +72,7 @@ function PageHome (){
             </section>
             <section>
                 <div className="img-gallery">
-                    <img src="/khanalukling_react/public/images/banner/LINE_ALBUM_3224_240214_1.jpg" alt="" />
+                    <img src={process.env.PUBLIC_URL+'/images/gallery/LINE_ALBUM_3224_240214_1.jpg'} width={'100%'} height={'150px'} alt="" />
                 </div>
                 <div className="text-gallery">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, sequi!
@@ -76,12 +80,21 @@ function PageHome (){
             </section>
             <section>
                 <div className="img-gallery">
-                    <img src="/khanalukling_react/public/images/banner/LINE_ALBUM_3224_240214_1.jpg" alt="" />
+                    <img src={process.env.PUBLIC_URL+'/images/gallery/LINE_ALBUM_3224_240214_1.jpg'} width={'100%'} height={'150px'} alt="" />
                 </div>
                 <div className="text-gallery">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, sequi!
                 </div>
             </section>
+            <section>
+                <div className="img-gallery">
+                    <img src={process.env.PUBLIC_URL+'/images/gallery/LINE_ALBUM_3224_240214_1.jpg'} width={'100%'} height={'150px'} alt="" />
+                </div>
+                <div className="text-gallery">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, sequi!
+                </div>
+            </section>
+            
         </article>
     </>);
 }
